67b72ab77abdc9d9f7fb127ec18b77a44fea8870
unknown <dafa.prasetyo2006@gmail.com>
2025-03-01 06:20:44 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import ProfilePage from "./pages/Profile.jsx";
+import ProfilePage from "./pages/profile.jsx";
 
 const router = createBrowserRouter([
   {
